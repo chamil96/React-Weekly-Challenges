c6ab00406c0c48e950113ee809fa26b682ffc821
Christopher Hamilton <naseemhamilton96@gmail.com>
2017-08-30 11:23:25 -0400

    Contains Very Easy and Easy

--- weeklychallenges/src/Header.js
+++ weeklychallenges/src/Header.js
@@ -1,4 +1,4 @@
-// EASY:  Pass a prop for "name" into the component you just created so the h1 tag reads, "Hello world, {name}" Please refer to App.js to see where name is plugged in
+
 
 import React, { Component } from 'react';
 
@@ -11,6 +11,7 @@ function Header(){
 }
 */
 
+// EASY:  Pass a prop for "name" into the component you just created so the h1 tag reads, "Hello world, {name}" Please refer to App.js to see where name is plugged in
 class Header extends Component {
   render() {
     const { name } = this.props;
